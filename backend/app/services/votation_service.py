5d0607e211184009a78547ad6f9c136332f96e19
Paolo Condello <kaochallengers7@gmail.com>
2026-01-21 12:53:34 +0100

    votation_service update

--- backend/app/services/votation_service.py
+++ backend/app/services/votation_service.py
@@ -3,6 +3,7 @@ from backend.app.models.user import User
 from sqlalchemy.orm import Session
 from app.models.food import Food
 from app.models.day import Day
+import backend.app.algo as algo
 
 # Here we implement function to submit votation
 def submit_votation(db: Session, voter_id: int, day_votes: list[int], food_votes: list[int]):
@@ -39,7 +40,10 @@ def submit_votation(db: Session, voter_id: int, day_votes: list[int], food_votes
     db.commit()
 
 
-
+current_avg_list_days = []
+current_mean_list_days = []
+current_avg_list_foods = []
+current_mean_list_foods = []
 # Now we inmplement the function that starts the process of average computation
 def init_avg_computation(db: Session):
     
@@ -50,14 +54,32 @@ def init_avg_computation(db: Session):
     days = db.query(Day).all()
     for day in days:
         day.current_avg = calculate_average(day.votes)
+        day.current_var = calculate_pvariance(day.votes)
+        current_avg_list_days.append(day.current_avg)
+        current_mean_list_days.append(day.current_var)
 
     foods = db.query(Food).all()
     for food in foods:
         food.current_avg = calculate_average(food.votes)
+        food.current_var = calculate_pvariance(food.votes)
+        current_avg_list_foods.append(food.current_avg)
+        current_mean_list_foods.append(food.current_var)
 
 
 def calculate_average(votes_list: list[int]) -> float:
     if not votes_list:
         return 0.0
-    pass
+    else:
+        return algo.calculate_average(votes_list)
+    
+def calculate_pvariance(votes_list: list[float]) -> float:
+    if not votes_list:
+        return 0.0
+    else:
+        return algo.calculate_pvariance(votes_list)
+    
+def final_choice_days(average: list[int], mean: list[float]) -> int:
+    return algo.final_choice(current_avg_list_days, current_mean_list_days)
 
+def final_choice_foods(average: list[int], mean: list[float]) -> int:
+    return algo.final_choice(current_avg_list_foods, current_mean_list_foods)
